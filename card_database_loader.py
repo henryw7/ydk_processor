6cfeb238e024b64ea95a55da10351690221e258c
Henry Wang <henrywang0142857@gmail.com>
2025-11-01 12:11:21 -0700

    Add card name to id map

--- card_database_loader.py
+++ card_database_loader.py
@@ -1,6 +1,7 @@
 import urllib.request
 import json
 import yaml
+import re
 from dateutil import parser
 
 url_ygoprodeck_version = "https://db.ygoprodeck.com/api/v7/checkDBVer.php"
@@ -9,9 +10,74 @@ url_ygoprodeck_all_sets = "https://db.ygoprodeck.com/api/v7/cardsets.php"
 
 cachefile_version = "data/version.yaml"
 cachefile_all_sets = "data/all_sets.yaml"
+cachefile_card_name_map = "data/card_name_to_id_map.yaml"
 cachefile_cards_template = "data/cards/%s.yaml"
 
 n_all_cards_split = 100
+
+special_character_to_english = {
+    'α': 'alpha',
+    'β': 'beta',
+    'γ': 'gamma',
+    'δ': 'delta',
+    'ε': 'epsilon',
+    'ζ': 'zeta',
+    'η': 'eta',
+    'θ': 'theta',
+    'ι': 'iota',
+    'κ': 'kappa',
+    'λ': 'lambda',
+    'μ': 'mu',
+    'ν': 'nu',
+    'ξ': 'xi',
+    'ο': 'omicron',
+    'π': 'pi',
+    'ρ': 'rho',
+    'σ': 'sigma',
+    'τ': 'tau',
+    'υ': 'upsilon',
+    'φ': 'phi',
+    'χ': 'chi',
+    'ψ': 'psi',
+    'ω': 'omega',
+    'Α': 'Alpha',
+    'Β': 'Beta',
+    'Γ': 'Gamma',
+    'Δ': 'Delta',
+    'Ε': 'Epsilon',
+    'Ζ': 'Zeta',
+    'Η': 'Eta',
+    'Θ': 'Theta',
+    'Ι': 'Iota',
+    'Κ': 'Kappa',
+    'Λ': 'Lambda',
+    'Μ': 'Mu',
+    'Ν': 'Nu',
+    'Ξ': 'Xi',
+    'Ο': 'Omicron',
+    'Π': 'Pi',
+    'Ρ': 'Rho',
+    'Σ': 'Sigma',
+    'Τ': 'Tau',
+    'Υ': 'Upsilon',
+    'Φ': 'Phi',
+    'Χ': 'Chi',
+    'Ψ': 'Psi',
+    'Ω': 'Omega',
+    '+': 'plus',
+    '＋': 'plus',
+    # '-': 'minus', # This is also dash
+    '−': 'minus',
+}
+
+def replace_special_characters(text):
+    converted = []
+    for char in text:
+        if char in special_character_to_english:
+            converted.append(' ' + special_character_to_english[char])
+        else:
+            converted.append(char)
+    return ''.join(converted)
 
 def print_json(json_obj):
     print(json.dumps(json_obj, indent = 4))
@@ -42,6 +108,11 @@ def load_card(card_id):
     with open(cachefile_cards, "r") as file_handle:
         cached_cards = yaml.safe_load(file_handle)
     return cached_cards[card_id]
+
+def load_card_name_to_id_map():
+    with open(cachefile_card_name_map, "r") as file_handle:
+        cached_card_name_map = yaml.safe_load(file_handle)
+    return cached_card_name_map
 
 if __name__ == '__main__':
     version_json = fetch_url(url_ygoprodeck_version)
@@ -90,6 +161,7 @@ if __name__ == '__main__':
     assert len(cards_json) > 0
     # print_json(cards_json)
 
+    cached_card_name_map = {}
     cached_cards_bundle = [{} for i in range(n_all_cards_split)]
     for card in cards_json:
         # print_json(card)
@@ -138,10 +210,21 @@ if __name__ == '__main__':
             "sets" : cached_card_sets,
         }
 
+        card_name_key = card_name
+        card_name_key = replace_special_characters(card_name_key)
+        card_name_key = re.sub(r'[^a-zA-Z0-9]', ' ', card_name_key)
+        card_name_key = re.sub(r'\s+', ' ', card_name_key.strip())
+        card_name_key = card_name_key.lower()
+        assert card_name_key not in cached_card_name_map, (card_name, card_name_key)
+        cached_card_name_map[card_name_key] = card_id
+
     for i_bundle in range(n_all_cards_split):
         cachefile_cards = cachefile_cards_template %(f"{i_bundle:03d}")
         with open(cachefile_cards, "w") as file_handle:
             yaml.dump(cached_cards_bundle[i_bundle], file_handle)
+
+    with open(cachefile_card_name_map, "w") as file_handle:
+        yaml.dump(cached_card_name_map, file_handle)
 
     cached_version["database_version"] = version_json["database_version"]
     cached_version["last_update"] = version_json["last_update"]
